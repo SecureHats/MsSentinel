42ece8b17eec81a41eb8ae8ef9c28bf3d644938a
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2022-02-16 16:06:59 +0100

    Create Get-MsSentinelContext.ps1

---
+++ Private/Get-MsSentinelContext.ps1
@@ -0,0 +1,49 @@
+param(
+    [Parameter(Mandatory=$true)]$ResourceGroup,
+    [Parameter(Mandatory=$true)]$Workspace,    
+    [Parameter(Mandatory=$true)]$Location
+)
+
+
+function CheckModules($module) {
+    $installedModule = Get-InstalledModule -Name $module -ErrorAction SilentlyContinue
+    if ($null -eq $installedModule) {
+        Write-Warning "The $module PowerShell module is not found"
+        #check for Admin Privleges
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+
+        if (-not ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+            #Not an Admin, install to current user
+            Write-Warning -Message "Can not install the $module module. You are not running as Administrator"
+            Write-Warning -Message "Installing $module module to current user Scope"
+            Install-Module -Name $module -Scope CurrentUser -Force
+            Import-Module -Name $module -Force
+        }
+        else {
+            #Admin, install to all users
+            Write-Warning -Message "Installing the $module module to all users"
+            Install-Module -Name $module -Force
+            Import-Module -Name $module -Force
+        }
+    }
+    #Install-Module will obtain the module from the gallery and install it on your local machine, making it available for use.
+    #Import-Module will bring the module and its functions into your current powershell session, if the module is installed.  
+}
+
+CheckModules("Az.Resources")
+CheckModules("Az.OperationalInsights")
+CheckModules("Az.SecurityInsights")
+CheckModules("Az.MonitoringSolutions")
+
+Write-Host "`r`nIf not logged in to Azure already, you will now be asked to log in to your Azure environment. `nFor this script to work correctly, you need to provide credentials of a Global Admin or Security Admin for your organization. `nThis will allow the script to enable all required connectors.`r`n" -BackgroundColor Magenta
+
+Read-Host -Prompt "Press enter to continue or CTRL+C to quit the script" 
+
+$context = Get-AzContext
+
+if(!$context){
+    Connect-AzAccount
+    $context = Get-AzContext
+}
+
+$script:SubscriptionId = $context.Subscription.Id
